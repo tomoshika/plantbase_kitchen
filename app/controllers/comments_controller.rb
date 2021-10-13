e705f2f6526cb980b0017292c36c9d9ab7a8fbad
tomoshika <tmm.orange.skm@gmail.com>
2021-10-13 04:50:34 +0000

    いいね機能追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    # @recipe = Recipe.find(params[:recipe_id])
+    # @comment = Comment.new(comment_params)
+    # recipe_comment = current_user.comments.new(comment_params)
+    # recipe_comment.recipe_id = @recipe.id
+    # recipe_comment.save
+    # redirect_to recipe_path(recipe)
+
     @recipe = Recipe.find(params[:recipe_id])
     @comment = Comment.new(comment_params)
-    recipe_comment = current_user.comments.new(comment_params)
-    recipe_comment.recipe_id = @recipe.id
-    recipe_comment.save
-    redirect_to recipe_path(recipe)
+    @comment.recipe_id = @recipe.id
+    @comment.user_id= current_user.id
+    if @comment.save
+     redirect_to recipe_path(@recipe)
+    else
+     render 'recipe/show'
+    end
   end
 
   def destroy
---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,16 @@
+class LikesController < ApplicationController
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    like = current_user.likes.new(recipe_id: recipe.id)
+    like.save
+    redirect_to recipe_path(recipe)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:recipe_id])
+    like = current_user.likes.find_by(recipe_id: recipe.id)
+    like.destroy
+    redirect_to recipe_path(recipe)
+  end
+
+end
