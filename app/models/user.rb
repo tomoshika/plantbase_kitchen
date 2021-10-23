02778c4844e5c00262185ec0e6a49b2358cb47e4
tomoshika <tmm.orange.skm@gmail.com>
2021-10-23 04:55:28 +0000

    ボタン編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # validates :name, presence: true
+
   attachment :image
   # mount_uploader :image, UserUploader
   has_many :recipes, dependent: :destroy
